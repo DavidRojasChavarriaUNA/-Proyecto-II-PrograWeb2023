68f101c713188eb7526a635f403f36945b62375f
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-05-26 00:38:59 -0600

    Rutas para votar
    Rutas para votar, se quitan las que no se requieren

--- routes.php
+++ routes.php
@@ -17,10 +17,9 @@
     Route::get('/votacion/(:number)/delete','VotacionController@destroy');
     Route::get('/votacion/(:number)/desactivar','VotacionController@cambiarEstado');
     
-    Route::get('/votante', 'VotanteController@index');
-    Route::get('/votante/(:number)/votar', 'VotanteController@votar');
-    Route::post('/votante/chooseOption', 'VotanteController@chooseOption');
-    Route::post('/votante/confirmOptionVote', 'VotanteController@confirmOptionVote');
+    Route::get('server/votante/(:number)', 'VotanteController@index');
+    Route::get('server/votante/(:number)/votar', 'VotanteController@votar');
+    Route::post('server/votante/confirmOptionVote', 'VotanteController@confirmOptionVote');
 
     Route::get('/resultados', 'ResultadosController@index');
     Route::get('/resultados/(:number)/votar', 'ResultadosController@votar');
