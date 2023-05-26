150e29984515cce8a09d2e16d8ba8befe88f493d
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-05-25 23:03:37 -0600

    Limpieza y funcionalidad para RestFull
    Se elimina métodos que ya no serán necesarios, y se habilita la respuesta para restfull

--- controllers/SeguridadController.php
+++ controllers/SeguridadController.php
@@ -4,86 +4,20 @@ include_once('./models/UserModel.php');
 include_once('./models/Codes.php');
 
 class  SeguridadController extends Controller{
-    public function showLoginForm() {
-        $mensaje = $_GET['mensaje'];
-        $error = false;
-        if(!isset($mensaje)){
-            $mensaje = "";
-        }else{
-            $error = true;
-        }
-        return view('seguridad/login', 
-        ['title'=>'Mi voto app - Autenticarse',
-         'error'=>$error,
-         'messaje'=>$mensaje,
-         'model' =>['email' => "",'password' => ""]]);
-     }
 
-     public function showRegistrationForm() {
-        return view('seguridad/register', 
-                 ['title'=>'Mi voto app - Registrarse',
-                 'login'=>Auth::check()]);
+    public function LoadAutenticatedUser($id){
+        $respuesta = UserModel::getUserById($id);
+        return $respuesta;
     }
-    public function register() {
-        $name = Input::get('name');  
-        $email = Input::get('email');  
-        $user = Input::get('user');  
-        $password = Input::get('password');
-        $item = [
-          'name'=>$name,'email'=>$email,  
-          'user'=>$user,'password'=>$password];
-        
-        $respuesta = UserModel::CreateUser($item);
-        if ($respuesta["Code"] == CodeSuccess) {
-          return redirect("/seguridad/showLoginForm");
-        }
-        $mensaje = "{$respuesta["Code"]} - {$respuesta["message"]}";
-        return redirect("/seguridad/registrationError?email={$email}&mensaje={$mensaje}");
-      }
 
-      public function registrationError() {
-        $name = $_GET['name']; 
-        $email = $_GET['email'];
-        $user = $_GET['user'];   
-        $mensaje = $_GET['mensaje'];
-        return view('seguridad/register',
-          ['title'=>'Mi voto app - Registrarse', 
-          'error'=>true,
-          'messaje'=>$mensaje,
-          'model' =>['email' => $email,'password' => ""]]);
+    public function register($userToRegister) {
+        $respuesta = UserModel::CreateUser($userToRegister);
+        return $respuesta;
     }
     
-    public function autenticate() {
-        $email = Input::get('email');   
-        $password = Input::get('password');
-        $userToAutenticate = ['email' => $email,'password' => $password];
+    public function autenticate($userToAutenticate) {
         $respuesta = UserModel::Autenticate($userToAutenticate);
-        if ($respuesta["Code"] == CodeSuccess) {
-          return redirect("/sitioInterno");
-        }
-        $mensaje = "{$respuesta["Code"]} - {$respuesta["message"]}";
-        return redirect("/seguridad/loginNotAuthorized?email={$email}&mensaje={$mensaje}");
-    }
-
-    public function loginNotAuthorized() {
-        $email = $_GET['email'];  
-        $mensaje = $_GET['mensaje'];
-        return view('seguridad/login',
-          ['title'=>'Mi voto - Autenticarse', 
-          'error'=>true,
-          'messaje'=>$mensaje,
-          'model' =>['email' => $email,'password' => ""]]);
-    }
-
-    public function logout() {
-        $respuesta = UserModel::logout();
-        if ($respuesta["Code"] == CodeSuccess) {
-            return redirect('/');
-        }
-        else{
-            $mensaje = "{$respuesta["Code"]} - {$respuesta["message"]}";
-            echo "<h1>{$mensaje}</h1>";
-        }
+        return $respuesta;
     }
 
 }
--- models/UserModel.php
+++ models/UserModel.php
@@ -8,12 +8,15 @@
 
     public static function Autenticate($userToAutenticate) {
         try {
+            if(!isset($userToAutenticate) || !isset($userToAutenticate["email"]) || !isset($userToAutenticate["password"]))
+                return ["Code" => CodeUnautorized, "message" => "Usuario y contraseña es requerido."];
+
             $result = Auth::attempt($userToAutenticate);
             if($result){
-              return ["Code" => CodeSuccess, "message" => ""];
+                return ["Code" => CodeSuccess, "message" => "Usuario autenticado"];
             }
             else{
-              return ["Code" => CodeUnautorized, "message" => "Usuario o contraseña inválido."];
+                return ["Code" => CodeUnautorized, "message" => "Usuario o contraseña inválido."];
             }
         }
         catch (Exception $e) {
@@ -21,48 +24,23 @@
         }
     }
 
-    public static function IsAutenticated() {
-      try {
-          $result = Auth::check();
-          if($result){
-            return ["Code" => CodeSuccess, "message" => ""];
-          }
-          else{
-            return ["Code" => CodeExpired, "message" => "La sesión ha cadudado"];
-          }
-      }
-      catch (Exception $e) {
-          Auth::logout();
-          return ["Code" => CodeError, "message" => "Ocurrió un error al validar si el usuario está autenticado, {$e->getMessage()}."];
-      }
-    }
     public static function CreateUser($item)
-  {
-    try {
-      self::create($item);
-      return ["Code" => 0, "message" => "Usuario creado con éxito."];
-    } catch (Exception $e) {
-      return ["Code" => 99, "message" => "No se pudo crear el usuario, {$e->getMessage()}."];
-    }
-  }
-
-    public static function logout() {
-      try {
-          Auth::logout();
-          return ["Code" => CodeSuccess, "message" => ""];
-      }
-      catch (Exception $e) {
-          Auth::logout();
-          return ["Code" => CodeError, "message" => "Ocurrió un error al cerrar la sesión del usuario, {$e->getMessage()}."];
-      }
+    {
+        try {
+            self::create($item);
+            return ["Code" => 0, "message" => "Usuario creado con éxito."];
+        } 
+        catch (Exception $e) {
+            return ["Code" => 99, "message" => "No se pudo crear el usuario, {$e->getMessage()}."];
+        }
     }
     
     public static function getUserById($id){
-      $users = self::find($id);
-      if(!empty($users)){
-          return ["Code" => CodeSuccess, "User" => $users[0]];
-      }
-      return ["Code" => CodeNotFound, "message" => "No se encontró un usuario con el id: {$id}."];
+        $users = self::find($id);
+        if(!empty($users)){
+            return ["Code" => CodeSuccess, "message" => "Usuario encontrado", "User" => $users[0]];
+        }
+        return ["Code" => CodeNotFound, "message" => "No se encontró un usuario con el id: {$id}."];
     }
 
   }
