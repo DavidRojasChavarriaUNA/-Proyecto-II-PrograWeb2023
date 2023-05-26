d72e2ee37a762905da47a10f9f0e3c821dd2c1ce
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-05-25 23:01:31 -0600

    Rutas
    Se actualiza las rutas, se elimina rutas de seguridad que ya no son necesarias, se agrega nuevas rutas accesibles desde la carpeta server

--- routes.php
+++ routes.php
@@ -1,14 +1,11 @@
 <?php
     
     Route::get('/', 'SitioPublicidadController@index');
-    Route::get('/seguridad/showLoginForm', 'SeguridadController@showLoginForm');
-    Route::post('/seguridad/autenticate', 'SeguridadController@autenticate');
-    Route::get('/seguridad/loginNotAuthorized', 'SeguridadController@loginNotAuthorized');
-    Route::get('/seguridad/logout', 'SeguridadController@logout');
+    Route::post('server/seguridad/register','SeguridadController@register'); 
+    Route::post('server/seguridad/autenticate', 'SeguridadController@autenticate');
+    Route::get('server/seguridad/(:number)', 'SeguridadController@LoadAutenticatedUser');
+
     Route::get('/sitioInterno', 'SitioInternoController@Home');
-    Route::get('/seguridad/showRegistrationForm', 'SeguridadController@showRegistrationForm');
-    Route::post('/seguridad/register','SeguridadController@register'); 
-    Route::get('/seguridad/registrationError', 'SeguridadController@registrationError');
     
     Route::resource('/votacion', 'VotacionController');
     Route::post('/votacion/newOption', 'VotacionController@newOption');
