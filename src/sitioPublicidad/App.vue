7726a02d64c3c4f288eaf393edf61d1071588e95
Fernanda-15 <fernandaqp15@gmail.com>
2023-05-28 17:42:43 -0600

    Componentes header y footer sitio publicidad

---
+++ src/sitioPublicidad/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header-publicidad></header-publicidad>
+  <!-- <main id="principal" class="container">
+      <section id="toastMensajes">
+        <toast-alert v-model="respuestaServicio" v-bind:respuestaServicio="respuestaServicio"></toast-alert>
+      </section>
+    <router-view v-on:mostrarMensaje="mostrarMensaje"></router-view>
+  </main> -->
+  <footer-publicidad></footer-publicidad>
+</template>
+
+<script>
+  import {Codigos} from '../js/sitioPublicidad'
+  import headerPublicidad from './headerPublicidad.vue'
+  import footerPublicidad from './footerPublicidad.vue'
+  import toastAlert from '../toadsAlerts/toastAlert.vue'
+
+  const urlBase = 'http://localhost:8089/server'
+
+  export default {
+    data() {
+      return {
+        message: 'Hello World!'
+      }
+    },
+    components: {
+      headerPublicidad,
+      footerPublicidad,
+      toastAlert
+    },
+    methods: {
+    }
+  }
+</script>
